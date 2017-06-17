6094585e246951dae7b34952e233e379a9ba9645
Sweeha Panwar <sweehapanwar22@gmail.com>
2017-06-17 16:16:34 +0530

    Temporary Solution

---
+++ IoTHub/DeviceIdentity/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+using System;
+using System.Threading.Tasks;
+
+namespace DeviceIdentity
+{
+    internal class Program
+    {
+        private static RegistryManager registryManager;
+        private static string connectionString = "HostName=SmartCityHack.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=GOkuUw/NCHJFBT6LO08cA1rPh75CtYlkXEDNvN3V46A=";
+
+        private static async Task AddDeviceAsync()
+
+        {
+            string deviceId = "myFirstSensor";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+
+        private static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IoTHub/Simulator/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator
+{
+    internal class Program
+    {
+        private static DeviceClient deviceClient;
+        private static string iotHubUri = "SmartCityHack.azure-devices.net";
+        private static string deviceKey = "sxqo2ubOGztF9aWdz+NULVLdCGiwygn7wU50o5Y5hXY=";
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double minCO2Emission = 5.0;
+            double minPopulation = 1326801576;
+            int messageId = 1;
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentCO2Emission = minCO2Emission + rand.NextDouble() * 8.91;
+                double currentPopulation = minPopulation + rand.NextDouble() * 2.43;
+
+                var telemetryDataPoint = new
+                {
+                    messageId = messageId++,
+                    deviceId = "myFirstSensor",
+                    C02Emission = currentCO2Emission,
+                    Population = currentPopulation
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+                message.Properties.Add("emissionAlert", (currentCO2Emission > 30) ? "true" : "false");
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                await Task.Delay(1000);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("myFirstSensor", deviceKey), TransportType.Mqtt);
+
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+        }
+    }
+}
